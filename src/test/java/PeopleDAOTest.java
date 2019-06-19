dc99cc3d40f5c20df30894dd89bfd9368f880428
sail45852 <sail456852@hotmail.com>
2019-06-19 15:44:27 +0800

    damn java connector version error

---
+++ src/test/java/PeopleDAOTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ * User: yz<br/>
+ * Date: 6/18/2019<br/>
+ * Time: 7:12 PM<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = JavaConfig.class)
+public class PeopleDAOTest {
+
+//    @Autowired
+//    private RedisRepository redisRepository;
+
+    @Test
+    public void listAll() {
+//        System.err.println("redisRepository = " + redisRepository);
+    }
+}
